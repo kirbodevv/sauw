3c88e740e92ebe659010d06d35ca4ca3872e60c1
Kirill <kirbo06@mail.ru>
2026-01-01 20:54:39 +0100

    refactor: remove unused BlockKind

--- src/world/block.rs
+++ src/world/block.rs
@@ -1,14 +1,6 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct BlockId(pub u16);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum BlockKind {
-    Air,
-    Grass,
-    Dirt,
-    Stone,
-}
-
 #[derive(Debug, Clone, Copy)]
 pub enum Layer {
     Ground,
