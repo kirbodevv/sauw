20e0884aeb736b3d3efd533992249025846e2afc
kirbodevv <kirbodevv@sect.cloud>
2026-06-21 22:03:54 +0200

    style: simplify if statement in `commands/craft.rs`

--- src/game/commands/craft.rs
+++ src/game/commands/craft.rs
@@ -15,16 +15,16 @@ pub fn craft(
     mut event_writer: MessageWriter<CraftItem>,
     recipe_registry: Option<Res<RecipeRegistry>>,
 ) {
-    if let Some(Ok(CraftCommand { recipe })) = log.take() {
-        if let Some(registry) = recipe_registry {
-            let id = registry.try_id_by_name(&recipe);
+    if let Some(Ok(CraftCommand { recipe })) = log.take()
+        && let Some(registry) = recipe_registry
+    {
+        let id = registry.try_id_by_name(&recipe);
 
-            let Some(recipe_id) = id else {
-                log.reply_failed(format!("recipe not found: {}", recipe));
-                return;
-            };
+        let Some(recipe_id) = id else {
+            log.reply_failed(format!("recipe not found: {}", recipe));
+            return;
+        };
 
-            event_writer.write(CraftItem { recipe_id });
-        }
+        event_writer.write(CraftItem { recipe_id });
     }
 }
