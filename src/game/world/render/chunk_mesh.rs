6b110a5f32d5b75520537d48045f63a74f44ff1f
kirbodevv <kirbodevv@sect.cloud>
2026-08-24 20:54:38 +0200

    refactor: extract mesh builder

--- src/game/world/render/chunk_mesh.rs
+++ src/game/world/render/chunk_mesh.rs
@@ -1,16 +1,13 @@
-use bevy::{
-    asset::RenderAssetUsages,
-    mesh::{Indices, PrimitiveTopology},
-    prelude::*,
-};
+use bevy::prelude::*;
 
 use crate::{
     constants::{CHUNK_SIZE, CHUNK_VOLUME, TILE_SIZE},
     game::{
         assets::atlas::{AtlasAsset, TextureId},
-        registry::block_registry::{BlockDefinition, BlockId, BlockRegistry},
+        registry::block_registry::{BlockId, BlockRegistry},
         world::generator::idx,
     },
+    shared::MeshBuilder,
 };
 
 pub fn build_ground_mesh(
@@ -18,9 +15,7 @@ pub fn build_ground_mesh(
     registry: &BlockRegistry,
     atlas: &AtlasAsset,
 ) -> Mesh {
-    let mut positions = Vec::new();
-    let mut uvs = Vec::new();
-    let mut indices = Vec::new();
+    let mut mesh_builder = MeshBuilder::default();
 
     for x in 0..CHUNK_SIZE {
         for y in 0..CHUNK_SIZE {
@@ -31,20 +26,14 @@ pub fn build_ground_mesh(
                 continue;
             }
 
-            append_quad(
-                block,
-                atlas,
-                x,
-                y,
-                0.0,
-                &mut positions,
-                &mut uvs,
-                &mut indices,
-            );
+            let (x, y) = (x as f32 * TILE_SIZE, y as f32 * TILE_SIZE);
+            let size = block.sprite_size;
+            let offset = block.sprite_offset;
+
+            mesh_builder.append_quad(TextureId::new(block.name), &atlas, x, y, 0.0, size, offset);
         }
     }
-
-    make_mesh(positions, uvs, indices)
+    mesh_builder.build()
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -68,64 +57,4 @@ pub fn spawn_chunk_mesh(
         MeshMaterial2d(material.clone()),
         Transform::from_xyz(0.0, 0.0, 0.0),
     ));
-}
-
-#[allow(clippy::too_many_arguments)]
-fn append_quad(
-    block: &BlockDefinition,
-    atlas: &AtlasAsset,
-    cx: usize,
-    cy: usize,
-    z: f32,
-    positions: &mut Vec<[f32; 3]>,
-    uvs: &mut Vec<[f32; 2]>,
-    indices: &mut Vec<u32>,
-) {
-    let texture_id = TextureId::new(block.name);
-    let atlas_entry = &atlas.entries[&texture_id];
-
-    let px = cx as f32 * TILE_SIZE;
-    let py = cy as f32 * TILE_SIZE;
-
-    let size = block.sprite_size;
-    let offset = block.sprite_offset;
-
-    let tex_x = atlas_entry.x() as f32;
-    let tex_y = atlas_entry.y() as f32;
-    let tex_w = atlas_entry.width() as f32;
-    let tex_h = atlas_entry.height() as f32;
-    let atlas_w = atlas.width as f32;
-    let atlas_h = atlas.height as f32;
-
-    let pad = 0.5;
-    let u0 = (tex_x + pad) / atlas_w;
-    let v0 = (tex_y + pad) / atlas_h;
-    let u1 = (tex_x + tex_w - pad) / atlas_w;
-    let v1 = (tex_y + tex_h - pad) / atlas_h;
-
-    let base = positions.len() as u32;
-
-    let cx = px + TILE_SIZE / 2.0 + offset.x;
-    let cy = py + TILE_SIZE / 2.0 + offset.y;
-    let hw = size.x / 2.0;
-    let hh = size.y / 2.0;
-
-    positions.extend_from_slice(&[
-        [cx - hw, cy - hh, z],
-        [cx + hw, cy - hh, z],
-        [cx - hw, cy + hh, z],
-        [cx + hw, cy + hh, z],
-    ]);
-    uvs.extend_from_slice(&[[u0, v1], [u1, v1], [u0, v0], [u1, v0]]);
-    indices.extend_from_slice(&[base, base + 1, base + 2, base + 2, base + 1, base + 3]);
-}
-
-fn make_mesh(positions: Vec<[f32; 3]>, uvs: Vec<[f32; 2]>, indices: Vec<u32>) -> Mesh {
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::default(),
-    )
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
-    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
-    .with_inserted_indices(Indices::U32(indices))
 }
---
+++ src/shared/mesh.rs
@@ -0,0 +1,70 @@
+use bevy::{
+    asset::RenderAssetUsages,
+    mesh::{Indices, PrimitiveTopology},
+    prelude::*,
+};
+
+use crate::game::assets::atlas::{AtlasAsset, TextureId};
+
+#[derive(Default)]
+pub struct MeshBuilder {
+    positions: Vec<[f32; 3]>,
+    uvs: Vec<[f32; 2]>,
+    indices: Vec<u32>,
+}
+
+impl MeshBuilder {
+    pub fn append_quad(
+        &mut self,
+        texture: TextureId,
+        atlas: &AtlasAsset,
+        x: f32,
+        y: f32,
+        z: f32,
+        size: Vec2,
+        offset: Vec2,
+    ) {
+        let atlas_entry = &atlas.entries[&texture];
+
+        let tex_x = atlas_entry.x() as f32;
+        let tex_y = atlas_entry.y() as f32;
+        let tex_w = atlas_entry.width() as f32;
+        let tex_h = atlas_entry.height() as f32;
+        let atlas_w = atlas.width as f32;
+        let atlas_h = atlas.height as f32;
+
+        let pad = 0.5;
+        let u0 = (tex_x + pad) / atlas_w;
+        let v0 = (tex_y + pad) / atlas_h;
+        let u1 = (tex_x + tex_w - pad) / atlas_w;
+        let v1 = (tex_y + tex_h - pad) / atlas_h;
+
+        let base = self.positions.len() as u32;
+
+        let x = x + size.x / 2.0 + offset.x;
+        let y = y + size.y / 2.0 + offset.y;
+        let hw = size.x / 2.0;
+        let hh = size.y / 2.0;
+
+        self.positions.extend_from_slice(&[
+            [x - hw, y - hh, z],
+            [x + hw, y - hh, z],
+            [x - hw, y + hh, z],
+            [x + hw, y + hh, z],
+        ]);
+        self.uvs
+            .extend_from_slice(&[[u0, v1], [u1, v1], [u0, v0], [u1, v0]]);
+        self.indices
+            .extend_from_slice(&[base, base + 1, base + 2, base + 2, base + 1, base + 3]);
+    }
+
+    pub fn build(&self) -> Mesh {
+        Mesh::new(
+            PrimitiveTopology::TriangleList,
+            RenderAssetUsages::default(),
+        )
+        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, self.positions.clone())
+        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, self.uvs.clone())
+        .with_inserted_indices(Indices::U32(self.indices.clone()))
+    }
+}
