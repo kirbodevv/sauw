422d9201a67be0b53ce8bd9b6fb818199df2dc6b
kirbodevv <kirbodevv@sect.cloud>
2026-08-25 17:39:03 +0200

    refactor: simplify mesh building

--- src/game/world/render/chunk_mesh.rs
+++ src/game/world/render/chunk_mesh.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
 use crate::{
-    constants::{CHUNK_VOLUME, TILE_SIZE},
+    constants::{CHUNK_VOLUME, GROUND_LAYER, TILE_SIZE},
     game::{
         assets::atlas::{AtlasAsset, TextureId},
         registry::block_registry::{BlockId, BlockRegistry},
@@ -11,26 +11,21 @@ use crate::{
 };
 
 pub fn build_ground_mesh(
-    chunk_blocks: &[BlockId; CHUNK_VOLUME],
+    blocks: &[BlockId; CHUNK_VOLUME],
     registry: &BlockRegistry,
     atlas: &AtlasAsset,
 ) -> Mesh {
     let mut builder = MeshBuilder::default();
 
-    for (x, y) in chunk_positions() {
-        let block_id = chunk_blocks[idx(x, y, 0)];
-        let block = registry.get(block_id);
-
-        if block_id.is_air() {
-            continue;
-        }
-
+    chunk_positions().for_each(|(x, y)| {
+        let id = blocks[idx(x, y, GROUND_LAYER)];
+        let block = registry.get(id);
         let position = Vec3::new(x as f32 * TILE_SIZE, y as f32 * TILE_SIZE, 0.0);
         let size = block.sprite_size;
         let offset = block.sprite_offset;
 
         builder.append_quad(TextureId::new(block.name), atlas, position, size, offset);
-    }
+    });
 
     builder.build()
 }
